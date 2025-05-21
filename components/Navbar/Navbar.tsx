a750b7de5904500e4ad89de4d63936d6fbc2eba6
Ibrahim Gazi <coder.ibrahimgazi@gmail.com>
2025-05-22 00:37:17 +0600

    Complete navigation menu

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -2,12 +2,15 @@
 
 import Link from 'next/link';
 import Image from 'next/image';
+import { Menu } from '@/theme-config';
+import { usePathname } from 'next/navigation';
 
 const Navbar = () => {
+    const pathname = usePathname();
 
     return (
         <section className="w-full bg-white border-b-1 border-[#e5e7eb] sticky top-0 z-50 shadow-sm">
-            <div className="max-w-7xl mx-auto px-2 py-3 flex items-center justify-start relative">
+            <div className="max-w-7xl mx-auto px-2 py-3 flex items-center justify-between relative">
                 <Link href="/" className="mr-10">
                     <Image
                         src="/images/logo.png"
@@ -18,6 +21,27 @@ const Navbar = () => {
                         priority
                     />
                 </Link>
+                <div className="flex items-center gap-2">
+                    {Menu.map((link, index) => {
+                        const isActive = link.path === pathname;
+                        return (
+                            <Link
+                                key={index}
+                                href={link.path}
+                                className={`flex items-center gap-1 px-4 py-2 rounded transition-all duration-300 ${isActive
+                                    ? 'text-[#009245] font-bold bg-slate-100 rounded-full'
+                                    : 'text-black hover:text-[#009245] hover:font-bold hover:bg-slate-100 hover:rounded-full'
+                                    }`}
+                            >
+                                {link.name}
+                            </Link>
+                        )
+                    })}
+                </div>
+
+                <div>
+                    
+                </div>
             </div>
         </section>
     );
---
+++ theme-config/Menu.ts
@@ -0,0 +1,9 @@
+const Menu = [
+    { name: "Home", path: "/" },
+    { name: "Blogs", path: "/blogs" },
+    { name: "Post Page", path: "/post" },
+    { name: "Contact", path: "/contact" },
+    { name: "About", path: "/about" },
+];
+
+export default Menu;
